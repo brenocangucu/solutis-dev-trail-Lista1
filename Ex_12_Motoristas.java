f950ccbec518caa116926c7abd71cbab344ea2bf
Breno Canguçu <brenocangucu@gmail.com>
2024-08-19 17:39:37 -0300

    Update Ex_12

--- Ex_12_Motoristas.java
+++ Ex_12_Motoristas.java
@@ -15,8 +15,51 @@ Todos os cálculos de média devem produzir resultados de ponto flutuante.
 
  */
 
+import java.util.Scanner;
+
 public class Ex_12_Motoristas {
     public static void main(String[] args){
+        //armazenar a quilometragem e o consumo total
+        int totalKmDirigidos = 0;
+        int totalLitrosDeGasolinaConsumidos = 0;
+        int contador = 1;
+        //para um tanque
+        int kmDirigidos;
+        int litrosDeGasolinaConsumidos;
+        double mediaKmLitro;
 
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            try{
+                System.out.println("Tanque " + contador);
+                System.out.println("Digite o número de kM dirigidos: (enter finaliza)");
+
+                String input = scanner.nextLine();
+
+                if(input.equals("")){
+                    System.out.println("Total de quilometros rodados: " + totalKmDirigidos + "\nTotal de litros de gasolina consumidos: " + totalLitrosDeGasolinaConsumidos + "\n");
+                    break;
+                }
+                kmDirigidos = Integer.parseInt(input);
+
+                System.out.println("Digite a quantidade de litros de gasolina consumidos:");
+                litrosDeGasolinaConsumidos = Integer.parseInt(scanner.nextLine());
+
+                mediaKmLitro = kmDirigidos * 1.0 / litrosDeGasolinaConsumidos;
+
+                System.out.println("Tanque " + contador + ", consumo: " + mediaKmLitro +  "km/l");
+
+                totalKmDirigidos += kmDirigidos;
+                totalLitrosDeGasolinaConsumidos += litrosDeGasolinaConsumidos;
+
+                System.out.println("Total de quilometros rodados: " + totalKmDirigidos + "\nTotal de litros de gasolina consumidos: " + totalLitrosDeGasolinaConsumidos + "\n");
+
+                contador++;
+            } catch (Exception e) {
+                System.out.println("Número inválido");
+            }
+
+        }
     }
 }
